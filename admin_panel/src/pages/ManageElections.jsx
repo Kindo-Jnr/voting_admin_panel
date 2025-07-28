aa6a07fa94ec7d7ab2e97b90bf6f13cb8bc2f663
kindo <kindojnr58@gmail.com>
2025-07-28 23:34:25 +0000

    topbar checked

--- admin_panel/src/pages/ManageElections.jsx
+++ admin_panel/src/pages/ManageElections.jsx
@@ -1,14 +1,14 @@
 import Sidebar from "../components/Sidebar";
 
 import ElectionsTable from "../components/ElectionsTable";
-import Topbar from "../components/Topbar";
+import TopBar from "../components/TopBar";
 
 export default function ManageElections() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gray-50 text-gray-800">
-        <Topbar />
+        <TopBar />
         <div className="p-6 flex-1 overflow-auto space-y-7">
           <ElectionsTable />
         </div>
